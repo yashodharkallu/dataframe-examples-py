51d395372a08c7f3e4a7cfee14b14a421b707684
SidharthaRay <sidhartha.ray533@gmail.com>
2020-09-17 19:03:21 +0530

    addint installation steps - ec2

--- utils/aws_utils.py
+++ utils/aws_utils.py
@@ -5,12 +5,6 @@ def get_redshift_jdbc_url(redshift_config: dict):
     database = redshift_config["redshift_conf"]["database"]
     username = redshift_config["redshift_conf"]["username"]
     password = redshift_config["redshift_conf"]["password"]
-    print(host)
-    print(port)
-    print(database)
-    print(username)
-    print(password)
-    print('jdbc:redshift://{}'.format(host))
     return "jdbc:redshift://{}:{}/{}?user={}&password={}".format(host, port, database, username, password)
 
 
@@ -18,4 +12,4 @@ def get_mysql_jdbc_url(mysql_config: dict):
     host = mysql_config["mysql_conf"]["hostname"]
     port = mysql_config["mysql_conf"]["port"]
     database = mysql_config["mysql_conf"]["database"]
-    return f"jdbc:mysql://{host}:{port}/{database}?autoReconnect=true&useSSL=false"
+    return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
