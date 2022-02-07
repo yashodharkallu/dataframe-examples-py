38a159a93d776282969cd74a8eecdd218e2dace2
ykkallu <yashodharkallu@gmail.com>
2022-02-07 08:34:17 +0530

    test

--- rdd/scholaship_recipient_join_filter.py
+++ rdd/scholaship_recipient_join_filter.py
@@ -1,13 +1,9 @@
-from pyspark.sql import SparkSession, Row
+from pyspark.sql import SparkSession
 from distutils.util import strtobool
 import os.path
 import yaml
 
 if __name__ == '__main__':
-
-    os.environ["PYSPARK_SUBMIT_ARGS"] = (
-        '--packages "org.apache.hadoop:hadoop-aws:2.7.4" pyspark-shell'
-    )
 
     # Create the SparkSession
     spark = SparkSession \
@@ -38,6 +34,9 @@ if __name__ == '__main__':
         .map(lambda line: line.split(",")) \
         .map(lambda lst: (int(lst[0]), (int(lst[1]), strtobool(lst[2]), lst[3], lst[4], strtobool(lst[5]), strtobool(lst[6]), int(lst[7]))))
 
+    for rec in demographics_pair_rdd.collect():
+        print(res)
+
     finances_pair_rdd = finances_rdd \
         .map(lambda line: line.split(",")) \
         .map(lambda lst: (int(lst[0]), (strtobool(lst[1]), strtobool(lst[2]), strtobool(lst[3]), int(lst[4]))))
@@ -45,7 +44,11 @@ if __name__ == '__main__':
     print('Participants belongs to \'Switzerland\', having debts and financial dependents,')
     join_pair_rdd = demographics_pair_rdd \
         .join(finances_pair_rdd) \
-        .filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1)) \
+
+    for rec in join_pair_rdd.collect():
+        print(res)
+
+    join_pair_rdd = join_pair_rdd.filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1)) \
 
     join_pair_rdd.foreach(print)
 
